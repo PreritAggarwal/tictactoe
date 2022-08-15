7de9874c38f85d9b85b0fd99f48337c2db457b46
Prerit Aggarwal <preritaggarwal66@gmail.com>
2022-08-15 10:40:50 +0530

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const app = () => {
+  return (
+  <>
+  <h1>Welcome to React Vite Micro App!</h1>
+  <p>Hard to get more minimal than this React app.</p>
+  </>);
+};
+export default app;
